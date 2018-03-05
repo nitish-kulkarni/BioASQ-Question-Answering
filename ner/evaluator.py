a864519486181b95bb5d9ad9b18edfa244fb3a77
Nitish Kulkarni <ni4lgi@gmail.com>
2018-03-05 12:02:26 -0500

    Fix bugs. Save serialized data instead of csv.

--- ner/evaluator.py
+++ ner/evaluator.py
@@ -47,15 +47,20 @@ def evaluate(tagger, data, multiple=False, snippets=False):
 
     return pd.DataFrame(results), missed
 
-def summary_from_results(results):
+def summary_from_results(results, data):
     list_type = results[results.type == 'list']
     factoid_type = results[results.type == 'factoid']
+    v_size = pd.DataFrame([{'qid': i.qid, 'V': i.V_snippets} for i in data.questions])
     rows = []
 
     for df, qtype in [(list_type, 'list'), (factoid_type, 'factoid')]:
         N = len(df) * 1.0
         exact_matched_questions = len(df[df.exact_matches > 0]) / N
         soft_match_questions = len(df[df.soft_matches > 0]) / N
+
+        joined = df.join(v_size.set_index('qid'), on=['qid'], lsuffix='', rsuffix='_r')
+        joined = joined[joined.V > 0]
+        frac = (joined.total_entities / joined.V).mean()
 
         n_exact_answers = df.total_answers.sum() * 1.0
         exact_matched_answers = df.exact_matches.sum() / n_exact_answers
@@ -67,9 +72,73 @@ def summary_from_results(results):
             'soft_match_questions': soft_match_questions,
             'exact_matched_answers': exact_matched_answers,
             'soft_match_answers': soft_match_answers,
+            'n_entities/total_tokens': frac,
         })
 
-    return pd.DataFrame(rows, columns=['type', 'exact_matched_questions', 'soft_match_questions', 'exact_matched_answers', 'soft_match_answers'])
+    return pd.DataFrame(rows, columns=['type', 'exact_matched_questions', 'soft_match_questions', 'exact_matched_answers', 'soft_match_answers', 'n_entities/total_tokens'])
+
+def clean_entities(df, data):
+    rows = []
+    for itr, row in df.iterrows():
+        entities = list(np.unique(_clean_tokens(row.entities)))
+        answers = row.answers
+        exact_matches = sum([answer in entities for answer in answers])
+        soft_matches = _soft_matches(answers, entities)
+        rows.append({
+            'qid': row.qid,
+            'answers': answers,
+            'entities': entities,
+            'exact_matches': exact_matches,
+            'total_answers': len(answers),
+            'total_entities': len(entities),
+            'soft_matches': soft_matches,
+            'type': row.type
+        })
+    
+    return pd.DataFrame(rows)
+
+def _clean_tokens(tokens):
+    return [i for i in tokens if len(re.sub('\\W+', '', i)) > 0]
+
+def ensemble_tags(results, typ='union'):
+    df = results[0]
+    if len(results) == 1:
+        return df
+
+    entities = {}
+    answers = {}
+    types = {}
+    first = True
+    for df2 in results[1:]:
+        for itr, row in df.join(df2.set_index('qid'), on=['qid'], lsuffix='_l', rsuffix='_r', how='inner').iterrows():
+            if row.qid not in entities:
+                entities[row.qid] = []
+            if first:
+                entities[row.qid].append(row.entities_l)
+            entities[row.qid].append(row.entities_r)
+            if row.qid not in answers:
+                answers[row.qid] = row.answers_l
+                types[row.qid] = row.type_l
+        first = False
+    
+    rows = []
+    for qid, answer in answers.items():
+        rows.append({
+            'qid': qid,
+            'answers': answer,
+            'type': types[qid],
+            'entities': _combine(entities[qid], typ=typ)            
+        })
+
+    return pd.DataFrame(rows)
+
+def _combine(arr, typ='union'):
+    sets = [set(i) for i in arr]
+    initial = sets[0]
+    for s in sets[1:]:
+        initial = initial.union(s) if typ == 'union' else initial.intersection(s)
+    return list(initial)
+
 
 def _soft_matches(answers, entities):
     answers = list(map(_normalize, answers))
